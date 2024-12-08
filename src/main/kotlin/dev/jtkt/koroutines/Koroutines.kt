cedd6743590333d9eaf2929b8eb25eac08ef3209
jtaub <JakeTaubner@ProtonMail.com>
2024-12-08 15:38:06 -0500

    documentation

--- src/main/kotlin/dev/jtkt/koroutines/Koroutines.kt
+++ src/main/kotlin/dev/jtkt/koroutines/Koroutines.kt
@@ -5,8 +5,14 @@ import java.util.concurrent.Executors
 
 private val defaultExecutor = Executors.newVirtualThreadPerTaskExecutor()
 
+/**
+ * Executes the [task] on a virtual thread.
+ */
 fun go(task: Runnable) =
     go(defaultExecutor, task)
 
+/**
+ * Executes the [task] on the given [executor].
+ */
 fun go(executor: Executor, task: Runnable) =
     executor.execute(task)
