803e8a3b7171ad48cc78da5d858eac579f34827e
jtaub <JakeTaubner@ProtonMail.com>
2024-12-08 15:41:48 -0500

    single thread executor test

--- src/test/kotlin/dev/jtkt/koroutines/KoroutinesKtTest.kt
+++ src/test/kotlin/dev/jtkt/koroutines/KoroutinesKtTest.kt
@@ -1,8 +1,10 @@
 package dev.jtkt.koroutines
 
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class KoroutinesKtTest {
 
@@ -22,6 +24,21 @@ class KoroutinesKtTest {
     }
 
     @Test
-    fun testGo() {
+    fun `should return results in expected order with single thread executor`() {
+        // Given
+        val results = mutableListOf<Int>()
+        val executor = Executors.newSingleThreadExecutor()
+
+        // When
+        for (i in 0 until 10) {
+            go(executor) {
+                results += i
+            }
+        }
+        Thread.sleep(5)
+
+        // Then
+        val expected = (0 until 10).toList()
+        assertEquals(expected, results)
     }
 }
