af85e9bd9d4c53a85b617b2c1e145ae8739dfe31
jtaub <JakeTaubner@ProtonMail.com>
2024-12-11 08:53:36 -0500

    add unit test

--- src/test/kotlin/dev/jtkt/koroutines/KoroutineUtilsKtTest.kt
+++ src/test/kotlin/dev/jtkt/koroutines/KoroutineUtilsKtTest.kt
@@ -3,6 +3,8 @@ package dev.jtkt.koroutines
 import java.util.concurrent.CompletableFuture
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.time.measureTime
 
 class KoroutineUtilsKtTest {
     @Test
@@ -24,7 +26,7 @@ class KoroutineUtilsKtTest {
     }
 
     @Test
-    fun mapAsync() {
+    fun `mapAsync should give expected results`() {
         // Given
         val input = listOf(1, 2, 3)
 
@@ -34,5 +36,17 @@ class KoroutineUtilsKtTest {
         // Then
         val expected = listOf(1, 4, 9)
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `mapAsync should run all transform concurrently`() {
+        // Given a list with 100 elements
+        val input = (0..100).toList()
+
+        // When we make each task sleep for 100 milliseconds,
+        val actual = measureTime { input.mapAsync { Thread.sleep(100) } }
+
+        // Then the total time taken should not be much more than 100 milliseconds.
+        assertTrue(actual.inWholeMilliseconds < 125)
     }
 }
